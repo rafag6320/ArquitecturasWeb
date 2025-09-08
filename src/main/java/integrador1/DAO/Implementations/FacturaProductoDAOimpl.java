a1d4c41ef7a77404dc822319bae3f713e0ece4f1
Juan Manuel Santa Cruz <juanmasantax@gmail.com>
2025-09-08 16:47:25 -0300

    Implementacion sql clase FacturaProductoDAOimpl

--- src/main/java/integrador1/DAO/Implementations/FacturaProductoDAOimpl.java
+++ src/main/java/integrador1/DAO/Implementations/FacturaProductoDAOimpl.java
@@ -1,11 +1,11 @@
 package integrador1.DAO.Implementations;
 
 import integrador1.DAO.Interfaces.FacturaProductoDAO;
+import integrador1.Entities.Factura;
 import integrador1.Entities.FacturaProducto;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FacturaProductoDAOimpl implements FacturaProductoDAO {
@@ -35,16 +35,51 @@ public class FacturaProductoDAOimpl implements FacturaProductoDAO {
 
     @Override
     public void insertar(FacturaProducto facturaProducto) {
+        String sql = "INSERT INTO FacturaProducto (idFactura, idProducto, cantidad) VALUES (?, ?, ?)";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, facturaProducto.getIdFactura());
+            ps.setInt(2, facturaProducto.getIdProducto());
+            ps.setInt(3, facturaProducto.getCantidad());
+            ps.executeUpdate();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<FacturaProducto> obtenerPorFactura(int idFactura) {
-        return List.of();
-    }
+        List<FacturaProducto> lista = new ArrayList<>();
 
+        String sql = "SELECT idFactura, idProducto, cantidad FROM Factura_Producto WHERE idFactura = ?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, idFactura);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                FacturaProducto fp = new FacturaProducto(
+                        rs.getInt("idFactura"),
+                        rs.getInt("idProducto"),
+                        rs.getInt("cantidad")
+                );
+                lista.add(fp);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
     @Override
     public void eliminar(int idFactura, int idProducto) {
+        String sql = "DELETE FROM FacturaProducto WHERE idFactura = ? AND idProducto = ?";
 
+        try(PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, idFactura);
+            ps.setInt(2, idProducto);
+            ResultSet rs = ps.executeQuery();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
